318e014b69f743d56319eb9456677bec0a8d7f10
jandershock <jordan.andershock@gmail.com>
2022-06-07 15:36:57 -0500

    Finished chore repository

---
+++ Roomates/Repositories/ChoreRepository.cs
@@ -0,0 +1,93 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Roommates.Models;
+
+namespace Roommates.Repositories
+{
+    internal class ChoreRepository : BaseRepository, IGenericRepository<Chore>
+    {
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT id, Name FROM Chore";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Chore> list = new List<Chore>();
+
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(reader.GetOrdinal("Id"));
+                            string name = reader.GetString(reader.GetOrdinal("Name"));
+
+                            Chore addChore = new Chore()
+                            {
+                                Id = id,
+                                Name = name
+                            };
+                            list.Add(addChore);
+                        }
+
+                        return list;
+                    }
+                }
+            }
+        }
+
+        public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Chore WHERE Id=@id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Chore chore = null;
+
+                        if (reader.Read())
+                        {
+                            chore = new Chore()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                            };
+                        }
+                        return chore;
+                    }
+                }
+            }
+        }
+
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Chore (Name)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                } 
+            }
+        }
+    }
+}
---
+++ Roomates/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    internal interface IGenericRepository<T>
+    {
+        List<T>  GetAll();
+        T GetById(int id);
+        void Insert(T obj);
+
+    }
+}
